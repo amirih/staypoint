42806f6da94645f008f0e3284e22849255a4f43e
amirih <hossein.amiri.enterprise@gmail.com>
2026-05-20 15:38:29 -0400

    detected staypoints samples added

--- code/main.py
+++ code/main.py
@@ -65,7 +65,7 @@ def evaluate(calculated_data_path = "data/v1/b2/sp2.csv",ground_truth_path="data
     print(f"Evaluation Score: {score}")
 
 if __name__ == "__main__":
-    funcs=[sw, hsw]
+    funcs=[hsw]
     time_thresholds = [100, 50, 25, 10, 5]
     distance_thresholds = [200, 100, 50]
     noiselevels = [0, 10, 25, 50]
