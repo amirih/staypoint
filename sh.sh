b820f942949bf24a82192c3362330e3a1f0f809a
Hossein Amiri <hossein.amiri.enterprise@gmail.com>
2026-05-22 16:01:33 -0400

    Delete sh.sh

--- sh.sh
+++
@@ -1,9 +0,0 @@
-cd ~/onone/onnuc/Research/staypoint/data/v1/detected_staypoints_noiselevel50_dropoutlevel3/algorithm_hsw
-grep -r '"f1": 0.9' .
-grep -r '"f1": 0.8' .
-grep -r '"f1": 0.7' .
-grep -r '"f1": 0.6' .
-grep -r '"f1": 0.5' .
-
-cp time_threshold_15_minutes_dist_threshold_200_meters.parquet staypoints.parquet
-cat time_threshold_15_minutes_dist_threshold_200_meters.parquet_score.json 
